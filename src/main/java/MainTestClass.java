191f77036fc33aba219ddfb3b774da6e719a936f
siinc <soba1210@gmail.com>
2019-08-02 15:15:44 +0300

    II. Простые сценарии в Appium Ex2: Создание метода Написать функцию, которая проверяет наличие текста “Search…” в строке поиска перед вводом текста и помечает тест упавшим, если такого текста нет.
    Результат выполнения задания нужно отдельным коммитом положить в git. В качестве ответа прислать txt файл со ссылкой на коммит. Если вам потребовалось несколько коммитов для выполнения одного задания - присылайте ссылки на все эти коммиты с комментариями.

---
+++ src/main/java/MainTestClass.java
@@ -0,0 +1,74 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class MainTestClass {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "9");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/siinc/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apk/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testKeywordSearchPresence(){
+
+        waitForElementAndClick(
+                By.id("search_container"),
+                "no search filed found",
+                5
+                );
+        WebElement searchField = waitForElementPresent(
+                By.id("search_src_text"),
+                "",
+                12
+        );
+
+        String keywordInSearchField = searchField.getAttribute("text");
+        Assert.assertEquals(
+                "another text in search field",
+                "Search…",
+                keywordInSearchField);
+    }
+
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClick(By by, String erroe_message, long timeInSeconds){
+        WebElement element = waitForElementPresent(by, erroe_message, timeInSeconds);
+        element.click();
+        return element;
+    }
+}
+
